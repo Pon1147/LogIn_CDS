648e1bdae7e797a375d0dc72174dbe07b6f94fb9
viklatao117 <khoale1147@gmail.com>
2025-03-24 17:59:19 +0700

    remove Angular Icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,15 @@
 import { Component } from '@angular/core';
-import { NgIcon, provideIcons } from '@ng-icons/core';
-import { featherAirplay } from '@ng-icons/feather-icons';
-import { heroUsers } from '@ng-icons/heroicons/outline';
+
+
+import { LogInComponent } from "../components/log-in/log-in.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NgIcon],
+  imports: [LogInComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  viewProviders: [provideIcons({ featherAirplay, heroUsers })]
 })
 export class AppComponent {
   title = 'LogIn-CDS';
