cd8693143a55661df11b9226efc1bb034ec4b436
viklatao117 <khoale1147@gmail.com>
2025-03-24 17:48:53 +0700

    Config Angular Icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { featherAirplay } from '@ng-icons/feather-icons';
+import { heroUsers } from '@ng-icons/heroicons/outline';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [NgIcon],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  viewProviders: [provideIcons({ featherAirplay, heroUsers })]
 })
 export class AppComponent {
   title = 'LogIn-CDS';
